2ac5524bd4f2590e230518258696c134a8812438
Nitin0603 <97080226+Nitin0603@users.noreply.github.com>
2022-01-17 18:14:52 +0530

    Add files via upload

---
+++ CodeWithOutput(Assignment - 2).py
@@ -0,0 +1,38 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================================= RESTART: D:\Assignments\Q1.py ============================================================================
+Length of given string : 35
+Reverse of given string : egaugnal evitisnes esac a si nohtyP
+Slice of given string : a case sensitive
+Slice of given string : Python is object oriented language
+Index of a in given string : 10
+After Removing spaces from given string : Pythonisacasesensitivelanguage
+
+============================================================================= RESTART: D:\Assignments\Q2.py ============================================================================
+Hey Nitin Here!
+My SID is 21105091 
+I am from ECE and my CGPA is 9.9
+
+============================================================================= RESTART: D:\Assignments\Q3.py ============================================================================
+a&b : 8
+a|b : 58
+a^b : 50
+a and b left shift with 2 bits = a : 224 and b : 40
+a right shift with 2 bits and b right shifts with 4 bits = a : 14 and b : 0
+
+============================================================================= RESTART: D:\Assignments\Q4.py ============================================================================
+Enter the first number : 12
+Enter the second number : 23
+Enter the third number : 24
+Largest number is 24
+
+============================================================================= RESTART: D:\Assignments\Q5.py ============================================================================
+Enter string : My name is nitin
+Yes
+
+============================================================================= RESTART: D:\Assignments\Q6.py ============================================================================
+Enter length of side 1st : 12
+Enter length of side 2nd : 23
+Enter length of side 3rd : 25
+Yes
