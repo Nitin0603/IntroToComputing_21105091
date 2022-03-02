eaf0487fa31d6c50091f313b9d33b40e5e4a09d2
Nitin0603 <97080226+Nitin0603@users.noreply.github.com>
2022-03-03 00:51:41 +0530

    Add files via upload

---
+++ CodeWithOutput(Assignment-5).py
@@ -0,0 +1,23 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: D:\Assignments_of_Computing\Assignment-5\Q-1.py ===========
+
+=========== RESTART: D:\Assignments_of_Computing\Assignment-5\Q-2.py ===========
+
+=========== RESTART: D:\Assignments_of_Computing\Assignment-5\Q-3.py ===========
+
+=========== RESTART: D:\Assignments_of_Computing\Assignment-5\Q-4.py ===========
+Unsorted List : [1, 5, 7, 3, 2, 72, 33, 4, 53, 5234, 234, 1, 124]
+Sorted List : [1, 1, 2, 3, 4, 5, 7, 33, 53, 72, 124, 234, 5234]
+
+=========== RESTART: D:\Assignments_of_Computing\Assignment-5\Q-5.py ===========
+Enter the list size (integer) : 5
+Enter(integer) element-1 : 2
+Enter(integer) element-2 : 5
+Enter(integer) element-3 : 3
+Enter(integer) element-4 : 2
+Enter(integer) element-5 : 2
+Sorted array : [2, 2, 2, 3, 5]
+Enter the number to find occurences of that number (integer) : 2
+Number of occurrences of element 2 is 3
